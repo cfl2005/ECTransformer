4a40bc7fe1ca4d63a195e5601812506bebba29cd
Felix CAI <104542475@qq.com>
2021-10-26 09:35:57 +0800

    Update clib.py

--- framework/server/clib.py
+++ framework/server/clib.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #coding:utf-8
 
-__author__ = 'xmxoxo<xmxoxo@qq.com>'
-
 import os
 import sys
 import logging
@@ -14,8 +12,6 @@ SSDB_PORT = 8888
 queue_in = 'queue_pipeline_in'
 queue_out = 'queue_pipeline_out'
 
-# 连接SSDB数据库
-# SSDB_IP='127.0.0.1', SSDB_PORT=8888
 def newSSDB(): 
     try:
         ssdb = pyssdb.Client(SSDB_IP, SSDB_PORT, None, 15 )
