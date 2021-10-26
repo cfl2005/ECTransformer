5e7c9785feb282860ee46ab855ac813f7f9569a7
Felix CAI <104542475@qq.com>
2021-10-26 11:14:01 +0800

    Update ECTrans_client_test.py

--- ECTrans_client_test.py
+++ ECTrans_client_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 #coding:utf-8
 
-__author__ = 'xmxoxo<xmxoxo@qq.com>'
 
 import argparse
 import os
@@ -23,33 +22,27 @@ from translate import *
 import framework.pipeline_demo.ECTrans_framework.ECTrans_client #import *
 
 
-parser = argparse.ArgumentParser(description='ECTrans框架客户端')
-parser.add_argument('--datafile', type=str, default="report/data_100.txt", help='数据文件')
+parser = argparse.ArgumentParser(description='ECTrans client')
+parser.add_argument('--datafile', type=str, default="report/data_100.txt", help='file')
 args = parser.parse_args()
 datafile = args.datafile
 
-# 启动客户端
 # python3 ECTrans.py --cmd=client --batch_size=128 --datafile=data_100.txt
 # python3 ECTrans.py --cmd=client --batch_size=8 --datafile=data_20.txt
 
-print('正在启动客户端...')
+print('start client...')
 
 client = ECTrans_Client(ip='127.0.0.1',
                     port=5557,
                     port_out=5560,)
 
-# 设置编码器
+
 txt_encode = lambda x: get_sample(x).numpy().tolist()
 client.set_encoder(txt_encode)
 
-print('正在发送数据...')
+print('send data...')
 sents = client.send(datafile)
-print('正在等待返回结果...')
-'''
-# 打印结果
-for sent in sents:
-    print(sent)            
-'''
+print('wait result...')
 print('total results :%d' % len(sents))
 
 
